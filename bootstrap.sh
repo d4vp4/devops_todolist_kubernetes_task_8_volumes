8dd634ed8effb1020a6b3fa7900cf50130209596
Danylo Povar <danila74povar@gmail.com>
2026-02-06 13:37:33 +0200

    task8.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+echo "Starting deployment..."
+
+kubectl apply -f .infrastructure/namespace.yml
+
+kubectl apply -f .infrastructure/pv.yml
+kubectl apply -f .infrastructure/pvc.yml
+
+kubectl apply -f .infrastructure/configiMap.yml
+kubectl apply -f .infrastructure/secret.yml
+
+kubectl apply -f .infrastructure/deployment.yml
+kubectl apply -f .infrastructure/service.yml
+
+kubectl apply -f .infrastructure/hpa.yml
+kubectl apply -f .infrastructure/daemonset.yml
+kubectl apply -f .infrastructure/cronjob.yml
+
+echo "Deployment completed successfully! Check pods in mateapp namespace."
