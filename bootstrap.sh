da858d5e84d647fae730bbd871228b44157a4f94
Danylo Povar <danila74povar@gmail.com>
2026-02-06 13:45:41 +0200

    task8.2

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,22 @@
 
 echo "Starting deployment..."
 
+# 1. Namespace
 kubectl apply -f .infrastructure/namespace.yml
 
+# 2. Storage
 kubectl apply -f .infrastructure/pv.yml
 kubectl apply -f .infrastructure/pvc.yml
 
-kubectl apply -f .infrastructure/configiMap.yml
+# 3. Configs & Secrets (Виправлена назва файлу тут!)
+kubectl apply -f .infrastructure/configMap.yml
 kubectl apply -f .infrastructure/secret.yml
 
+# 4. App & Network
 kubectl apply -f .infrastructure/deployment.yml
 kubectl apply -f .infrastructure/service.yml
 
+# 5. Background tasks
 kubectl apply -f .infrastructure/hpa.yml
 kubectl apply -f .infrastructure/daemonset.yml
 kubectl apply -f .infrastructure/cronjob.yml
